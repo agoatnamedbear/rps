91c99cac41ebd17239a15943ad3e2c61bff10153
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-24 13:55:00 -0400

    Refactor

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-// make variable to hold computer choices
-// make a function to get a random choice from choices; return computerChoice
-// make a function that gets the player choice via prompt and make input lowercased; Check to make sure that the choice is a valid one
-// set variables for playerScore & computerScore
-// make playRound function; use the rps logic
-// log round winner
-// increment scores
-// make variables for playerSelection & computerSelection set to the get choices functions
-// call playRound with the selections as parameters
-// make function playGame that plays 5 rounds of playRound
-// move playRound and score variables into playGame function
-// log scores and declare a winner
-
 const computerChoices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+// Get computer selection
+const computerChoice = () => {
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
+
+// Get player selection
+const getPlayerChoice = () => {
+  let playerChoice = prompt("Rock Paper or Scissors?").toLowerCase();
+  if (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    return "Please enter a valid choice!";
+  } else {
+    return playerChoice;
+  }
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    return "Player wins this round";
+  } else {
+    computerScore++;
+    return "Computer wins this round";
+  }
+};
+
+const playGame = () => {
+  while (playerScore < 5 && computerScore < 5) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = computerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    console.log(`Player Selection: ${playerSelection}`);
+    console.log(`Computer Selection: ${computerSelection}`);
+    console.log(`Round Result: ${roundResult}`);
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Player Wins the Game!");
+  } else {
+    console.log("Computer Wins the Game!");
+  }
+};
+
+playGame();
