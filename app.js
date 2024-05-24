ddadf6d26a2acb6e41da7bf40e3b0e503e065190
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-24 11:20:38 -0400

    Pseudocode and computerChoices variable array

---
+++ app.js
@@ -0,0 +1,14 @@
+// make variable to hold computer choices
+// make a function to get a random choice from choices; return computerChoice
+// make a function that gets the player choice via prompt and make input lowercased; Check to make sure that the choice is a valid one
+// set variables for playerScore & computerScore
+// make playRound function; use the rps logic
+// log round winner
+// increment scores
+// make variables for playerSelection & computerSelection set to the get choices functions
+// call playRound with the selections as parameters
+// make function playGame that plays 5 rounds of playRound
+// move playRound and score variables into playGame function
+// log scores and declare a winner
+
+const computerChoices = ["rock", "paper", "scissors"];
