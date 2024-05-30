77daf04ef5d23d327a11809dab793aa306f9aecd
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-30 18:29:39 -0400

    Setup playerChoice function

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ let playerScore = 0;
 let computerScore = 0;
 const buttons = document.querySelectorAll(".btn");
 
-// Get computer selection
+// Get computer choice
 const computerChoice = () => {
   return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 
-// Get player selection
+// Get player choice
+const playerChoice = () => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      let playerSelection = button.value;
+      console.log(playerSelection);
+      return playerSelection;
+    });
+  });
+};
+playerChoice();
