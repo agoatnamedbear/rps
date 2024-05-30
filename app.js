4e868e683ae82dbbbd2ae7ea0f3613f47d932a1e
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-30 18:45:41 -0400

    Recode playerChoice function to call playRound inside; Add console.logs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const computerChoices = ["rock", "paper", "scissors"];
+
 let playerScore = 0;
 let computerScore = 0;
 const buttons = document.querySelectorAll(".btn");
@@ -7,15 +8,33 @@ const buttons = document.querySelectorAll(".btn");
 const computerChoice = () => {
   return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
+const computerSelection = computerChoice();
 
 // Get player choice
 const playerChoice = () => {
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       let playerSelection = button.value;
-      console.log(playerSelection);
+      console.log(`Player chose ${playerSelection}`);
+      console.log(`Computer chose ${computerSelection}`);
+      playRound(playerSelection, computerSelection);
       return playerSelection;
     });
   });
 };
+
 playerChoice();
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    console.log("Tie");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log("You win!");
+  } else {
+    console.log("You lose!");
+  }
+};
