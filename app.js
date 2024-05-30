2cc4838caf549a5fe75cc9a7c0a8b39875e42dbf
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-30 16:14:27 -0400

    Rework player choice to accept input from buttons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const computerChoices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll(".btn");
 
 // Get computer selection
 const computerChoice = () => {
@@ -8,53 +9,51 @@ const computerChoice = () => {
 };
 
 // Get player selection
-const getPlayerChoice = () => {
-  let playerChoice = prompt("Rock Paper or Scissors?").toLowerCase();
-  if (
-    playerChoice !== "rock" &&
-    playerChoice !== "paper" &&
-    playerChoice !== "scissors"
-  ) {
-    return "Please enter a valid choice!";
-  } else {
-    return playerChoice;
-  }
+let playerChoice = () => {
+  let choice;
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      choice = e.target.value;
+      console.log(button.value);
+    });
+  });
+  return choice;
 };
+playerChoice();
+// const playRound = (playerSelection, computerSelection) => {
+//   if (playerSelection === computerSelection) {
+//     return "It's a draw!";
+//   } else if (
+//     (playerSelection === "rock" && computerSelection === "scissors") ||
+//     (playerSelection === "paper" && computerSelection === "rock") ||
+//     (playerSelection === "scissors" && computerSelection === "paper")
+//   ) {
+//     playerScore++;
+//     return "Player wins this round";
+//   } else {
+//     computerScore++;
+//     return "Computer wins this round";
+//   }
+// };
 
-const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection) {
-    return "It's a draw!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    playerScore++;
-    return "Player wins this round";
-  } else {
-    computerScore++;
-    return "Computer wins this round";
-  }
-};
+// const playGame = () => {
+//   while (playerScore < 5 && computerScore < 5) {
+//     const playerSelection = playerChoice();
+//     const computerSelection = computerChoice();
+//     const roundResult = playRound(playerSelection, computerSelection);
 
-const playGame = () => {
-  while (playerScore < 5 && computerScore < 5) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = computerChoice();
-    const roundResult = playRound(playerSelection, computerSelection);
+//     console.log(`Player Selection: ${playerSelection}`);
+//     console.log(`Computer Selection: ${computerSelection}`);
+//     console.log(`Round Result: ${roundResult}`);
+//     console.log(`Player Score: ${playerScore}`);
+//     console.log(`Computer Score: ${computerScore}`);
+//   }
 
-    console.log(`Player Selection: ${playerSelection}`);
-    console.log(`Computer Selection: ${computerSelection}`);
-    console.log(`Round Result: ${roundResult}`);
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-  }
+//   if (playerScore > computerScore) {
+//     console.log("Player Wins the Game!");
+//   } else {
+//     console.log("Computer Wins the Game!");
+//   }
+// };
 
-  if (playerScore > computerScore) {
-    console.log("Player Wins the Game!");
-  } else {
-    console.log("Computer Wins the Game!");
-  }
-};
-
-playGame();
+// playGame();
