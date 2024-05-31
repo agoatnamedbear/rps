bad9ff2fef2fbc6b92cb562fe18e4a568fbfaf75
Sean Wildman <agoatnamedbear@gmail.com>
2024-05-31 09:03:41 -0400

    Code resetGame() & gameOver() functions

--- app.js
+++ app.js
@@ -1,40 +1,80 @@
-const computerChoices = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll(".btn");
+const playrScore = document.querySelector(".plyrScore");
+const compScore = document.querySelector(".compScore");
+const roundWinner = document.querySelector(".round_winner");
+const gameWinner = document.querySelector(".game_winner");
+const reset = document.querySelector(".resetBtn");
 
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll(".btn");
 
 // Get computer choice
 const computerChoice = () => {
-  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 };
-const computerSelection = computerChoice();
 
 // Get player choice
 const playerChoice = () => {
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       let playerSelection = button.value;
-      console.log(`Player chose ${playerSelection}`);
-      console.log(`Computer chose ${computerSelection}`);
-      playRound(playerSelection, computerSelection);
-      return playerSelection;
+      let computerSelection = computerChoice();
+      playGame(playerSelection, computerSelection);
     });
   });
 };
-
 playerChoice();
-
-const playRound = (playerSelection, computerSelection) => {
+const playGame = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    console.log("Tie");
+    roundWinner.textContent = "It's a tie!";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    console.log("You win!");
+    roundWinner.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
   } else {
-    console.log("You lose!");
+    roundWinner.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
   }
+  playrScore.textContent = playerScore;
+  compScore.textContent = computerScore;
+
+  gameOver();
+
+  if (playerScore === 5) {
+    playerScore = 0;
+    computerScore = 0;
+    gameWinner.textContent = "You won the game!";
+  } else if (computerScore === 5) {
+    playerScore = 0;
+    computerScore = 0;
+    gameWinner.textContent = "You lost the game!";
+  } else {
+    gameWinner.textContent = "";
+  }
+
+  resetGame();
+};
+
+const gameOver = () => {
+  if (playerScore === 5 || computerScore === 5) {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+  }
+};
+
+const resetGame = () => {
+  reset.addEventListener("click", () => {
+    playrScore.textContent = 0;
+    compScore.textContent = 0;
+    roundWinner.textContent = "";
+    gameWinner.textContent = "";
+    buttons.forEach((button) => {
+      button.disabled = false;
+    });
+  });
 };
